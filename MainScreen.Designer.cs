f72cd140aba08a6a9922cdd22b4d48adef948792
Dương Nhân <40598538+duongtainhan@users.noreply.github.com>
2018-12-03 02:11:15 +0700

    Resize window

--- MainScreen.Designer.cs
+++ MainScreen.Designer.cs
@@ -30,13 +30,13 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainScreen));
             this.panelMenuVertical = new System.Windows.Forms.Panel();
+            this.buttonDashBoard = new System.Windows.Forms.Button();
             this.panelTitleTop = new System.Windows.Forms.Panel();
             this.pictureBoxMinimize = new System.Windows.Forms.PictureBox();
             this.pictureBoxSizeWindow = new System.Windows.Forms.PictureBox();
             this.pictureBoxShutdown = new System.Windows.Forms.PictureBox();
             this.pictureBoxMenu = new System.Windows.Forms.PictureBox();
             this.panelContainer = new System.Windows.Forms.Panel();
-            this.buttonDashBoard = new System.Windows.Forms.Button();
             this.panelMenuVertical.SuspendLayout();
             this.panelTitleTop.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxMinimize)).BeginInit();
@@ -53,8 +53,25 @@
             this.panelMenuVertical.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.panelMenuVertical.Location = new System.Drawing.Point(0, 0);
             this.panelMenuVertical.Name = "panelMenuVertical";
-            this.panelMenuVertical.Size = new System.Drawing.Size(250, 800);
+            this.panelMenuVertical.Size = new System.Drawing.Size(250, 900);
             this.panelMenuVertical.TabIndex = 0;
+            // 
+            // buttonDashBoard
+            // 
+            this.buttonDashBoard.FlatAppearance.BorderSize = 0;
+            this.buttonDashBoard.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(48)))));
+            this.buttonDashBoard.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonDashBoard.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonDashBoard.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.buttonDashBoard.Image = ((System.Drawing.Image)(resources.GetObject("buttonDashBoard.Image")));
+            this.buttonDashBoard.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.buttonDashBoard.Location = new System.Drawing.Point(0, 94);
+            this.buttonDashBoard.Name = "buttonDashBoard";
+            this.buttonDashBoard.Size = new System.Drawing.Size(250, 49);
+            this.buttonDashBoard.TabIndex = 0;
+            this.buttonDashBoard.Text = "DashBoard";
+            this.buttonDashBoard.UseVisualStyleBackColor = true;
+            this.buttonDashBoard.Click += new System.EventHandler(this.buttonDashBoard_Click);
             // 
             // panelTitleTop
             // 
@@ -65,7 +82,7 @@
             this.panelTitleTop.Dock = System.Windows.Forms.DockStyle.Top;
             this.panelTitleTop.Location = new System.Drawing.Point(250, 0);
             this.panelTitleTop.Name = "panelTitleTop";
-            this.panelTitleTop.Size = new System.Drawing.Size(1150, 59);
+            this.panelTitleTop.Size = new System.Drawing.Size(1250, 59);
             this.panelTitleTop.TabIndex = 1;
             this.panelTitleTop.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panelTitleTop_MouseDown);
             // 
@@ -73,7 +90,7 @@
             // 
             this.pictureBoxMinimize.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.pictureBoxMinimize.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxMinimize.Image")));
-            this.pictureBoxMinimize.Location = new System.Drawing.Point(963, 12);
+            this.pictureBoxMinimize.Location = new System.Drawing.Point(1063, 12);
             this.pictureBoxMinimize.Name = "pictureBoxMinimize";
             this.pictureBoxMinimize.Size = new System.Drawing.Size(43, 41);
             this.pictureBoxMinimize.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -85,7 +102,7 @@
             // 
             this.pictureBoxSizeWindow.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.pictureBoxSizeWindow.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxSizeWindow.Image")));
-            this.pictureBoxSizeWindow.Location = new System.Drawing.Point(1031, 12);
+            this.pictureBoxSizeWindow.Location = new System.Drawing.Point(1131, 12);
             this.pictureBoxSizeWindow.Name = "pictureBoxSizeWindow";
             this.pictureBoxSizeWindow.Size = new System.Drawing.Size(43, 41);
             this.pictureBoxSizeWindow.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -97,7 +114,7 @@
             // 
             this.pictureBoxShutdown.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.pictureBoxShutdown.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxShutdown.Image")));
-            this.pictureBoxShutdown.Location = new System.Drawing.Point(1095, 12);
+            this.pictureBoxShutdown.Location = new System.Drawing.Point(1195, 12);
             this.pictureBoxShutdown.Name = "pictureBoxShutdown";
             this.pictureBoxShutdown.Size = new System.Drawing.Size(43, 41);
             this.pictureBoxShutdown.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -122,36 +139,20 @@
             this.panelContainer.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panelContainer.Location = new System.Drawing.Point(250, 59);
             this.panelContainer.Name = "panelContainer";
-            this.panelContainer.Size = new System.Drawing.Size(1150, 741);
+            this.panelContainer.Size = new System.Drawing.Size(1250, 841);
             this.panelContainer.TabIndex = 2;
-            // 
-            // buttonDashBoard
-            // 
-            this.buttonDashBoard.FlatAppearance.BorderSize = 0;
-            this.buttonDashBoard.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(48)))));
-            this.buttonDashBoard.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.buttonDashBoard.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.buttonDashBoard.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.buttonDashBoard.Image = ((System.Drawing.Image)(resources.GetObject("buttonDashBoard.Image")));
-            this.buttonDashBoard.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.buttonDashBoard.Location = new System.Drawing.Point(0, 94);
-            this.buttonDashBoard.Name = "buttonDashBoard";
-            this.buttonDashBoard.Size = new System.Drawing.Size(250, 49);
-            this.buttonDashBoard.TabIndex = 0;
-            this.buttonDashBoard.Text = "DashBoard";
-            this.buttonDashBoard.UseVisualStyleBackColor = true;
-            this.buttonDashBoard.Click += new System.EventHandler(this.buttonDashBoard_Click);
             // 
             // MainScreen
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1400, 800);
+            this.ClientSize = new System.Drawing.Size(1500, 900);
             this.Controls.Add(this.panelContainer);
             this.Controls.Add(this.panelTitleTop);
             this.Controls.Add(this.panelMenuVertical);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "MainScreen";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.panelMenuVertical.ResumeLayout(false);
             this.panelTitleTop.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxMinimize)).EndInit();
